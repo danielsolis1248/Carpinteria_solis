74524e645c046486cfedd13b5feee601806ed2a7
danielsolis1248 <itic18.danie.solis@iunis.edu.mx>
2021-11-17 13:49:50 -0600

    Cambio de foto de portada e desarrollo de correo

---
+++ correo.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($_POST['botonEnviar'])) {
+    #Validacion del boton
+    if (!empty($_POST['nombreApellido']) && !empty($_POST['correoElectronico']) && !empty($_POST['telefono']) && !empty($_POST['mensaje'])) {
+        $nombreApellido = $_POST['nombreApellido'];
+        $correoElectronico = $_POST['correoElectronico'];
+        $telefono = $_POST['telefono'];
+        $mensaje = $_POST['mensaje'];
+
+        $header = "From: itic18.daniel.solis@iunis.edu.mx" . "\r\n";
+        $header.= "Reply-To: itic18.daniel.solis@iunis.edu.mx" . "\r\n";
+        $header.= "X-Mailer: PHP/". phpversion();
+        $mail = mail($correoElectronico, $nombreApellido, $telefono, $mensaje);
+
+        if ($mail) {
+            echo "<h4>¡Correo enviado exitosamente!</h4>"
+        }
+
+
+    }
+}
+
+?>
